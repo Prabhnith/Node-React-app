7b5cdd78444f18d088f59ec9c2d986146a65f1ae
prabhnith <prabhjot.nith@gmail.com>
2018-12-22 23:23:27 +0530

    initial comment; react redux node

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: "1058458784554-v5ruk7mhmvppe5g0njiecontpn66u4qc.apps.googleusercontent.com",
+    googleClientSecret: "xHv2ERnj7uqjZU8UEcLBQBji",
+    mongoURI: "mongodb://prabhnith:password1@ds141674.mlab.com:41674/nodeapp",
+    cookieKey: "thisisthesecretkeyofourapp"
+}
---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    googleId: String,
+    name: String,
+    email: String
+});
+
+mongoose.model('users', UserSchema);
